5e06e12a7975da0b6fa697ae54e0600f58d904a4
simara86 <simara861011@gamil.com>
2022-06-30 12:13:30 -0400

    Spring all configured

--- P3/project3/src/main/java/com/revature/models/Categories.java
+++ P3/project3/src/main/java/com/revature/models/Categories.java
@@ -1,5 +1,11 @@
 package com.revature.models;
 
-public class Categories {
+import javax.persistence.Entity;
+
+import org.springframework.stereotype.Component;
+
+
+public enum Categories {
+	Walking,Grooming,DayCare;
 
 }
