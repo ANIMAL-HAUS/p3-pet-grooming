b42e806876f24607c1d4740bccabe3fbeb8c7e4e
simara86 <simara861011@gamil.com>
2022-06-30 16:35:33 -0400

    Product done

--- P3/project3/src/main/java/com/revature/controllers/ProductController.java
+++ P3/project3/src/main/java/com/revature/controllers/ProductController.java
@@ -1,4 +1,14 @@
 package com.revature.controllers;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.revature.models.Categories;
+import com.revature.models.Product;
 import com.revature.models.Users;
 import com.revature.services.ProductServices;
 import com.revature.services.UserServices;
@@ -10,42 +20,44 @@ public class ProductController {
 	public ProductController(ProductServices product) {
 		ps = product;
 	}
-	@PostMapping("/insertProductConsole")
-	public static ResponseEntity<Product> insertProductConsole(@RequestBody Product product){
-		System.out.println(product.getId());
-		
-		if(product.getgName() == null) {
-			return ResponseEntity.status(HttpStatus.NO_CONTENT).body(product);
-		}else {
-			product.setProduct(Categories.Console);
-			ProductServices.insertProduct(product);
-			return ResponseEntity.status(HttpStatus.NO_CONTENT).body(product);
-		}	
-	}
-	@PostMapping("/insertProductControllers")
-	public static ResponseEntity<Product> insertProductController(@RequestBody Product product){
-		System.out.println(product.getId());
-		
-		if(product.getgName() == null) {
-			return ResponseEntity.status(HttpStatus.NO_CONTENT).body(product);
-		}else {
-			product.setProduct(Categories.Controllers);
-			ProductServices.insertProduct(product);
-			return ResponseEntity.status(HttpStatus.NO_CONTENT).body(product);
-		}	
-	}
-	@PostMapping(" ")
-	public static ResponseEntity<Product> insertProductGames(@RequestBody Product product){
-		System.out.println(product.getId());
-		
-		if(product.getgName() == null) {
-			return ResponseEntity.status(HttpStatus.NO_CONTENT).body(product);
-		}else {
-			product.setProduct(Categories.Games);
-			ProductServices.insertProduct(product);
-			return ResponseEntity.status(HttpStatus.NO_CONTENT).body(product);
-		}	
-	}
+	
+	
+//	@PostMapping("/insertProductWalking")
+//	public static ResponseEntity<Product> insertProductWalking(@RequestBody Product product){
+//		System.out.println(product.getId());
+//		
+//		if(product.getProductType() == null) {
+//			return ResponseEntity.status(HttpStatus.NO_CONTENT).body(product);
+//		}else {
+//			product.setProductType(Categories.Walking);
+//			ProductServices.insertProduct(product);
+//			return ResponseEntity.status(HttpStatus.NO_CONTENT).body(product);
+//		}	
+//	}
+//	@PostMapping("/insertProductSitting ")
+//	public static ResponseEntity<Product> insertProductSitting(@RequestBody Product product){
+//		System.out.println(product.getId());
+//		
+//		if(product.getProductType() == null) {
+//			return ResponseEntity.status(HttpStatus.NO_CONTENT).body(product);
+//		}else {
+//			product.setProductType(Categories.Sitting);
+//			ProductServices.insertProduct(product);
+//			return ResponseEntity.status(HttpStatus.NO_CONTENT).body(product);
+//		}	
+//	}
+//		@PostMapping("/insertProductGrooming ")
+//		public static ResponseEntity<Product> insertProductGrooming(@RequestBody Product product){
+//			System.out.println(product.getId());
+//			
+//			if(product.getProductType() == null) {
+//				return ResponseEntity.status(HttpStatus.NO_CONTENT).body(product);
+//			}else {
+//				product.setProductType(Categories.Grooming);
+//				ProductServices.insertProduct(product);
+//				return ResponseEntity.status(HttpStatus.NO_CONTENT).body(product);
+//			}	
+//	}
 	@PostMapping("/getProductByID")
 	public static ResponseEntity<Product> getProductByID(@RequestBody int id){
 		Product p = new Product();
