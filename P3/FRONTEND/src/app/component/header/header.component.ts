4083694e120253c9cbd90969f6644700ab6511ad
Cody Taylor Jenkins <codytaylorjenkins@Codys-MacBook-Pro.local>
2022-07-13 15:59:46 -0500

    hover on buttons in header added

--- P3/FRONTEND/src/app/component/header/header.component.ts
+++ P3/FRONTEND/src/app/component/header/header.component.ts
@@ -1,6 +1,7 @@
 
 
 import { Component, OnInit } from '@angular/core';
+import { Users } from 'src/app/models/Users';
 
 
 @Component({
@@ -10,7 +11,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   // public totalItem : number = 0;
-
+user: Users;
+id: 0;
   constructor() { }
 
   ngOnInit(): void {
